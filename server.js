b15356053b0326cd7d18153a2f01ceddd971e7d5
tariqsohail <62976920+tariqsohail@users.noreply.github.com>
2020-09-22 19:02:16 -0700

    adds budget-tracker

---
+++ server.js
@@ -0,0 +1,30 @@
+  
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://user1:asdfgh1234@cluster0.ely5l.azure.mongodb.net/BudgetTracker?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
